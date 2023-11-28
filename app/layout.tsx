d24f93b15bf8784b062faa64b3682471dc6f9010
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-11-28 22:43:46 +0600

    prettier setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Abu Shyed Hemel",
-  description: "React Developer",
+  title: 'Abu Shyed Hemel',
+  description: 'React Developer',
 };
 
 export default function RootLayout({
@@ -15,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full'></div>
+        {children}
+      </body>
     </html>
   );
 }
