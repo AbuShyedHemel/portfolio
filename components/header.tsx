b106b1c7a38b1bf1a59582fa015677e0db2954eb
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-12-04 21:56:33 +0600

    Add liberies
    Changes:
    1. Make Header responsive
    2. Add framer-motion

---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+const Header = () => {
+  return (
+    <header className='z-[999] relative'>
+      <motion.div
+        className='fixed -translate-x-1/2 top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white bg-white border-opacity-40 shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
+        initial={{ y: -100, x: '-50%', opacity: 0 }}
+        animate={{ y: 0, x: '-50%', opacity: 1 }}
+      ></motion.div>
+    </header>
+  );
+};
+
+export default Header;
