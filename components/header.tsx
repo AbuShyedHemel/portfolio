de9f2a705e61b1be08b89f41b903a2297c0134d1
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-12-07 00:30:25 +0600

    add navigation animation and add navigation responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { links } from '@/lib/data';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -10,6 +12,26 @@ const Header = () => {
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
       ></motion.div>
+
+      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gr-500 sm:w-[initial] sm:flex-nowrap sm:ga[-5'>
+          {links?.map((link) => (
+            <motion.li
+              key={link.hash}
+              className='h-3/4 flex items-center justify-center'
+              animate={{ y: 0, opacity: 1 }}
+              initial={{ y: -100, opacity: 0 }}
+            >
+              <Link
+                href={link.hash}
+                className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
